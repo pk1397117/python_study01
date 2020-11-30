a32e58e5576ea9444bc211823986baee2cb03845
Administrator <997843624@163.com>
2020-11-30 11:23:24 +0800

    第七次修改

---
+++ study01/day07/07-装饰器详解.py
@@ -0,0 +1,28 @@
+import time
+
+
+def cal_time(fn):
+    print("我是外部函数，我被调用了")
+    print("fn = {}".format(fn))
+
+    def inner(n):
+        start = time.time()
+        s = fn(n)
+        end = time.time()
+        print("代码耗时:", end - start)
+        return s
+
+    return inner
+
+
+@cal_time
+def demo(n):
+    x = 0
+    for i in range(1, n):
+        x += i
+    # print(x)
+    return x
+
+
+m = demo(100000000)
+print("m的值是", m)

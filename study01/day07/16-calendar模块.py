084db2277bc3f85042ddf484bc385869f3d55932
zhangsan <997843624@qq.com>
2020-12-14 00:48:22 +0800

    第九次修改

--- study01/day07/16-calendar模块.py
+++ study01/day07/16-calendar模块.py
@@ -1,12 +1,4 @@
 import calendar  # 日历模块
-import io
 
-try:
-    c = calendar.calendar(2022)
-    file = open("F:/zzz/calendar.txt", "w")
-    file.write(c)
-    #read = open("F:/zzz/calendar.txt", "r")
-    #print(file.read())
-finally:
-    file.close()
-    #read.close()
+c = calendar.calendar(2022)
+print(c)
